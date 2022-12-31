9216202bfbb3ed911e5bc79d72f93e5b106e446c
federico <fede_fleitas@hotmail.com>
2022-12-30 21:55:46 -0300

    nuevos cambios

--- script.js
+++ script.js
@@ -3,14 +3,14 @@
 
 
 ///////////// Objeto constructor para base de las motos
-function Moto (nombre, precio, info, cash, cuotas,id){
-    this.nombre=nombre;
-    this.precio=precio;
-    this.info=info;
-    this.cash=cash;
-    this.cuotas=cuotas;
-    this.id=id;
-    
+function Moto(nombre, precio, info, cash, cuotas, id) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.info = info;
+    this.cash = cash;
+    this.cuotas = cuotas;
+    this.id = id;
+
 }
 
 
@@ -31,262 +31,86 @@ const productos = [fz, cb, bajaj, leon]
 const contenedorProductos = document.getElementById('contenedorProductos');
 
 productos.forEach((producto) => {
-  const divProducto = document.createElement('div');
-  divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
-  divProducto.innerHTML = `
-                          <div>
+    const divProducto = document.createElement('div');
+    divProducto.classList.add('card', 'col-xl-3', 'col-md-6', 'col-sm-12');
+    divProducto.innerHTML = `<div>
                               <div class="card-body" id="card${producto.id}">
                                   <h3 class="card-title"> ${producto.nombre} </h3>
                                   <p class="card-text">Precio total: $${producto.precio} </p>
                                   <button id="boton${producto.cash}" class="btn btn-primary"> Precio en efectivo </button>
                                   <button id="boton${producto.cuotas}" class="btn btn-primary"> Precio en cuotas </button>
-                                  <button id="boton${producto.info}" class="btn btn-primary"> Mas informacion </button>
-                              </div>
-                          </div>`;
-  contenedorProductos.appendChild(divProducto);
+                                  <button id="boton${producto.info}" class="btn btn-primary"> Mas informacion </button></div>
+                                  <div id="info${producto.id}"></div>
+                                  </div>`;
+    contenedorProductos.appendChild(divProducto);
 
-// CTA MAS INFORMACION
-  const boton1 = document.getElementById(`boton${producto.info}`);
-  const contenedor = document.getElementById(`card${producto.id}`)
+    // CTA MAS INFORMACION
+    const boton1 = document.getElementById(`boton${producto.info}`);
+    const contenedor = document.getElementById(`info${producto.id}`)
 
-  boton1.addEventListener('click', () => {
+    boton1.addEventListener('click', () => {
+    contenedor.innerHTML = "";
     contenedor.innerHTML += `<div>
-        <p>${producto.info}</p>
-        </div>`
-  });
-  /*boton1.addEventListener('mouseup', () => {
-    contenedor.innerHTML += "-"
-  });
-  */
-
-
-// CTA EFECTIVO 
-  const boton2 = document.getElementById(`boton${producto.cash}`);
-  const contenedor2 = document.getElementById(`card${producto.id}`)
-
-  boton2.addEventListener('click', () => {
-    contenedor2.innerHTML += `<div>
-        <p>El precio en efectivo tiene un descuento del 10% <br>
-        Precio final: $${producto.cash}</p>
-        </div>`
-  });
-  /*boton1.addEventListener('mouseup', () => {
-    contenedor.innerHTML += "-"
-  });
-  */
-
-// CTA CUOTAS
-  const boton3 = document.getElementById(`boton${producto.cuotas}`);
-  const contenedor3 = document.getElementById(`card${producto.id}`)
-
-  boton3.addEventListener('click', () => {
-    contenedor3.innerHTML += `<div>
-    <p>3 cuotas sin interes: $${producto.cuotas/3} <br>
-    6 cuotas sin interes: $${producto.cuotas/6} <br>
-    12 cuotas sin interes: $${producto.cuotas/12}</p>
+    <p>${producto.info}</p>
     </div>`
-  });
-  /*boton1.addEventListener('mouseup', () => {
-    contenedor.innerHTML += "-"
-  });
-  */
+    });
+    
 
+
+    // CTA EFECTIVO 
+    const boton2 = document.getElementById(`boton${producto.cash}`);
+
+    boton2.addEventListener('click', () => {
+    contenedor.innerHTML = "";
+    contenedor.innerHTML += `<div>
+    <p>El precio en efectivo tiene un descuento del 10% <br>
+    Precio final: $${producto.cash}</p>
+    </div>`
+    });
+    
+
+    // CTA CUOTAS
+    const boton3 = document.getElementById(`boton${producto.cuotas}`);
+
+    boton3.addEventListener('click', () => {
+    contenedor.innerHTML = "";
+    contenedor.innerHTML += `<div>
+    <p>3 cuotas sin interes: $${producto.cuotas / 3} <br>
+    6 cuotas sin interes: $${producto.cuotas / 6} <br>
+    12 cuotas sin interes: $${producto.cuotas / 12}</p>
+    </div>`
+    });
+    
 
 });
 
-///////////////////////////////// FUNCIONES
+
+//   FORMULARIO POR DOM 
+
+const formulario = document.getElementById('formContacto');
+const divForm = document.createElement('div');
+
+divForm.classList.add('mb-3');
+divForm.innerHTML = `<div id="formCard">
+                    <h5 class="text-center" >Completa con tu correo electrónico y consulta. </h5>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Correo electronico</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@hotmail.com"></div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Consulta</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></div>
+                        <button type="submit" class="btn btn-primary" id="botonSubmit">Enviar</button></div>`;
+formulario.appendChild(divForm);
 
 
+// mensaje de submit
 
-///funcion para elegir el medio de pago
-/*
-function elegirPago(precio){
-    alert(`El valor total de la unidad es de: $ ${precio}`)
-let medio = parseInt(prompt(`Seleecione la opción correspondiente a su medio de pago
+const botonSub = document.getElementById(`botonSubmit`);
+    const contenedor = document.getElementById(`formCard`)
 
-1: Efectivo (10% de descuento)
-
-2: Tarjeta de credito
-
-`))
-
-switch (medio) {
-    case 1:
-        precioEfectivo(precio)
-        break;
-    case 2:
-        precioCuotas(precio)
-        break;
-    default:
-        alert("La opcion ingresada no es correcta.")
-        break;
-}
-}
-
-function precioEfectivo(precio) {
-    let efectivo = precio * porcentaje
-alert(`Valor al contado: $${efectivo}`)
-}
-
-function precioCuotas(precio){
-    for (let i = 1; i <= 4; i++) {
-        let cuotas = i*3
-        let resultado = precio / cuotas
-        alert(`${cuotas} cuotas de $ ${resultado}`)
-    }
-    /*const datosTarjeta = [] //array para los datos de la tarjeta
-    let tarjeta = prompt("Ingrese los 16 digitos de su tarjeta")
-    datosTarjeta.push(tarjeta)
-    let codigo = prompt("Ingrese los 3 digitos del codigo de seguridad de su tarjeta")
-    datosTarjeta.push(codigo)
-    let dni = prompt("Ingrese el DNI del titular de la tarjeta ingresada")
-    datosTarjeta.push(dni)
-
-    alert("A continuación le detallaremos los datos ingresados. Si los mismos son correctos nos comunicaremos con usted a la brevedad para finalizar la operación")
-    alert(datosTarjeta[0])
-    alert(datosTarjeta[1])
-    alert(datosTarjeta[2])
-    alert("Muchas gracias por confiar en nosotros")
-    */
-
-/*
-function info(cilindrada,peso){
-    alert(`Su cilindrada es: ${cilindrada} y su peso es de: ${peso} `)
-}
-
-
-
-////////////////////////////////////////////////
-
-function consulta() {
-    let consulta = prompt(`Seleccione el modelo a cotizar:
-
-1: ${fz.nombre}
-    
-2: ${cb.nombre}
-    
-3: ${bajaj.nombre}
-    
-4: ${leon.nombre}
-`)
-    
-    switch (consulta) {
-        case "1":
-            info(fz.cc,fz.peso)
-            elegirPago(fz.precio)
-            break;
-        case "2":
-            info(cb.cc,cb.peso)
-            elegirPago(cb.precio)
-            break;
-        case "3":
-            info(bajaj.cc,bajaj.peso)
-            elegirPago(bajaj.precio)
-            break;
-        case "4":
-            info(leon.cc,leon.peso)
-            elegirPago(leon.precio)
-            break;
-    
-        default:
-            alert("La opcion ingresada no es correcta.")
-            break;
-    }  
-}
-*/
-//consulta()
-
-
-//
-/*
-let nuevoBoton = document.createElement("button")
-nuevoBoton.setAttribute("class", "btn btn-primary")
-nuevoBoton.setAttribute("id", "bot1")
-nuevoBoton.textContent = ("Precio en efectivo")
-
-let nuevoBoton2 = document.createElement("button")
-nuevoBoton2.setAttribute("class", "btn btn-primary")
-nuevoBoton2.setAttribute("id", "bot2")
-nuevoBoton2.textContent = ("Precio en cuotas")
-
-let nuevoBoton3 = document.createElement("button")
-nuevoBoton3.setAttribute("class", "btn btn-danger")
-nuevoBoton3.setAttribute("id", "bot3")
-nuevoBoton3.textContent = ("Mas información")
-
-
-document.querySelector(".tarjfz").appendChild(nuevoBoton3)
-document.querySelector(".tarjfz").appendChild(nuevoBoton)
-document.querySelector(".tarjfz").appendChild(nuevoBoton2)
-
-const productos = [fz]
-const contenedor = document.getElementById('tj1');
-
-nuevoBoton3.addEventListener("mousemove", () =>{
-    productos.forEach(producto => {
-        contenedor.innerHTML += `<div>
-        <p>Cilindrada: ${producto.cc}<br>
-        Peso:${producto.peso}</p>
-        </div>`
+    botonSub.addEventListener('click', () => {
+    contenedor.innerHTML += `<div>
+    <p>Sus datos han sido enviados. Muchas gracias</p>
+    </div>`
     });
-})
-
-nuevoBoton3.addEventListener("mouseover", () =>{
-    contenedor.innerHTML += ""
-    });
-
-*/
-
-
-
-/*let nuevoBoton3 = document.createElement("button")
-nuevoBoton3.setAttribute("class", "btn btn-primary")
-nuevoBoton3.setAttribute("id", "bot3")
-nuevoBoton3.textContent = ("Precio en efectivo")
-
-let nuevoBoton4 = document.createElement("button")
-nuevoBoton4.setAttribute("class", "btn btn-primary")
-nuevoBoton4.setAttribute("id", "bot4")
-nuevoBoton4.textContent = ("Precio en cuotas")
-
-
-
-document.querySelector(".tarjhd").appendChild(nuevoBoton3)
-document.querySelector(".tarjhd").appendChild(nuevoBoton4)
-
-
-
-let nuevoBoton5 = document.createElement("button")
-nuevoBoton5.setAttribute("class", "btn btn-primary")
-nuevoBoton5.setAttribute("id", "bot5")
-nuevoBoton5.textContent = ("Precio en efectivo")
-
-let nuevoBoton6 = document.createElement("button")
-nuevoBoton6.setAttribute("class", "btn btn-primary")
-nuevoBoton6.setAttribute("id", "bot6")
-nuevoBoton6.textContent = ("Precio en cuotas")
-
-
-
-document.querySelector(".tarjbaj").appendChild(nuevoBoton5)
-document.querySelector(".tarjbaj").appendChild(nuevoBoton6)
-
-
-let nuevoBoton7 = document.createElement("button")
-nuevoBoton7.setAttribute("class", "btn btn-primary")
-nuevoBoton7.setAttribute("id", "bot7")
-nuevoBoton7.textContent = ("Precio en efectivo")
-
-let nuevoBoton8 = document.createElement("button")
-nuevoBoton8.setAttribute("class", "btn btn-primary")
-nuevoBoton8.setAttribute("id", "bot6")
-nuevoBoton8.textContent = ("Precio en cuotas")
-
-
-
-document.querySelector(".tarjben").appendChild(nuevoBoton7)
-document.querySelector(".tarjben").appendChild(nuevoBoton8)
-*/
-
-
 
